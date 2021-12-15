708a44921e727a1fed2f0c9cf4329d78dc325329
eeroolli <eero.olli@gmail.com>
2021-12-15 14:54:15 +0100

    fix(env) installing needed resources

---
+++ env/setup_python.py
@@ -0,0 +1,11 @@
+# This script installs resources that are needed, and not successfully
+# installed in to the environment by the requirements.txt
+
+!pip install opencv-python
+!pip install tensorflow
+!pip install tensorboard
+!sudo apt install ffprobe
+!sudo apt install ffmpg
+
+
+
