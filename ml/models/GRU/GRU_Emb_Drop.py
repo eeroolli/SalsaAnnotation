988feb523d4ed26b7723b621e43549244cf8f7a4
abergues <berguesana@gmail.com>
2021-12-16 11:09:59 +0100

    feat(model) Updating GRU model template

--- ml/models/GRU/GRU_Emb_Drop.py
+++ ml/models/GRU/GRU_Emb_Drop.py
@@ -1,10 +1,54 @@
-# Try to import Time distributed layer
-import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras import models, layers
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import logging
+import os
+import glob
+
+if not os.path.exists('temp'):
+    os.makedirs('temp')
+
+Sim_ID = "GRU-64-32-Dense-16" # TODO: define as external parameter
+file_to_delete = glob.glob('temp/' + Sim_ID + '*')
+
+# Iterate over the list of filepaths & remove each file.
+for filePath in file_to_delete:
+    try:
+        os.remove(filePath)
+    except:
+        print("Error while deleting file : ", filePath)
+
+logging.basicConfig(filename='temp/' + Sim_ID + '.log', level=10)
+
+# Load the data
+PATH_DATA_TRAIN = "Data_train_validate/Data_train_norm.csv"
+PATH_DATA_VAL = "Data_train_validate/Data_val_norm.csv"
+data_train = pd.read_csv(PATH_DATA_TRAIN)
+data_val = pd.read_csv(PATH_DATA_VAL)
+
+# Define hyperparameters
+BATCH_SIZE = 8
+EPOCHS = 100
+MAX_SEQ_LENGTH = 40   # number of frames per figure
+NUM_FEATURES = 75     # number of join coordinates
+
+feat_cols = ['nose_x', 'nose_y',
+       'neck_x', 'neck_y', 'rshoulder_x', 'rshoulder_y', 'relbow_x',
+       'relbow_y', 'rwrist_x', 'rwrist_y', 'lshoulder_x', 'lshoulder_y',
+       'lelbow_x', 'lelbow_y', 'lwrist_x', 'lwrist_y', 'midhip_x', 'midhip_y',
+       'rhip_x', 'rhip_y', 'rknee_x', 'rknee_y', 'rankle_x', 'rankle_y',
+       'lhip_x', 'lhip_y', 'lknee_x', 'lknee_y', 'lankle_x', 'lankle_y',
+       'reye_x', 'reye_y', 'leye_x', 'leye_y', 'rear_x', 'rear_y', 'lear_x',
+       'lear_y', 'lbigtoe_x', 'lbigtoe_y', 'lsmalltoe_x', 'lsmalltoe_y',
+       'lheal_x', 'lheal_y', 'rbigtoe_x', 'rbigtoe_y', 'rsmalltoe_x',
+       'rsmalltoe_y', 'rheal_x', 'rheal_y', 'nose_v', 'neck_v', 'rshoulder_v',
+       'relbow_v', 'rwrist_v', 'lshoulder_v', 'lelbow_v', 'lwrist_v',
+       'midhip_v', 'rhip_v', 'rknee_v', 'rankle_v', 'lhip_v', 'lknee_v',
+       'lankle_v', 'reye_v', 'leye_v', 'rear_v', 'lear_v', 'lbigtoe_v',
+       'lsmalltoe_v', 'lheal_v', 'rbigtoe_v', 'rsmalltoe_v', 'rheal_v']
+
+assert len(feat_cols) == NUM_FEATURES
 
 # Fixing format of the label
 def enc_label(label):
@@ -19,40 +63,27 @@ def enc_label(label):
         code = 4
     return code
 
-# Define hyperparameters
-BATCH_SIZE = 8
-EPOCHS = 150
-MAX_SEQ_LENGTH = 40   # number of frames per figure
-NUM_FEATURES = 75     # number of join coordinates
-no_sample = 20        # number of examples
-
-# Import the data
-PATH_DATA_TRAIN = "Data_train_validate/Data_train_norm.csv"
-PATH_DATA_VAL = "Data_train_validate/Data_val_norm.csv"
-data_train = pd.read_csv(PATH_DATA_TRAIN)
-data_val = pd.read_csv(PATH_DATA_VAL)
-
-
 # Function to select a number of frames per figure and right in the correct format for the mdoel
-
 def transf_data(data):
-    # Data preprocessing, get the input X and the label y
+    # Get the input X and the label y
     ind_start = data[data['status'] == "S"].index.tolist()
     ind_end = data[data['status'] == "E"].index.tolist()
 
     # Take intervals between consecutive "S", they define one figure
     X = []
     y = []
+    info = []
 
     for i in range(len(ind_start) - 1):
-        X.append(data.iloc[ind_start[i]: ind_end[i], 4:-3])  # the last 25 (visibility ) + 2
+        X.append(data.loc[ind_start[i]: ind_end[i], feat_cols])
         y.append(data.loc[ind_start[i], 'label'])
+        info.append(data.loc[ind_start[i], ['clip_name', 'frame_nr']])
 
     # select frames from the interval
     ind_samp = []
 
     for i in range(len(ind_start) - 1):
-        # Take frames that are evenlly distributed
+        # Take frames that are evenly distributed
         aux = np.linspace(ind_start[i]
                           , ind_end[i]
                           , MAX_SEQ_LENGTH
@@ -67,24 +98,30 @@ def transf_data(data):
     X = [x.loc[ind_samp[ind], :].to_numpy() for (ind, x) in enumerate(X)]
     X = np.array(X)
     X = X.reshape(len(ind_start) - 1, MAX_SEQ_LENGTH, NUM_FEATURES).astype("float32")
-    # TODO: decide of the X values need to be normalized
     y = [enc_label(x) for x in y]
     y = np.array(y).astype("float32")
 
-    return X, y
+    return X, y, info
 
-# Train set
-X_train, y_train = transf_data(data_train)
-X_val, y_val = transf_data(data_val)
+# Training and validation sets
+X_train, y_train, info_train = transf_data(data_train)
+X_val, y_val, info_val = transf_data(data_val)
 
-# Build the model
+# print parameters to file
+logging.info(f"Paramters of the model BATCH_SIZE {BATCH_SIZE}")
+logging.info(f"Paramters of the model EPOCHS {EPOCHS}")
+logging.info(f"Paramters of the model MAX_SEQ_LENGTH {MAX_SEQ_LENGTH}")
+logging.info(f"Paramters of the model NUM_FEATURES {NUM_FEATURES}")
+
+# Build the model (This section can be modified to a diferent model)
 model = models.Sequential()
 model.add(layers.InputLayer(input_shape=(MAX_SEQ_LENGTH, NUM_FEATURES)))
-model.add(layers.GRU(128, return_sequences=True))
-model.add(layers.Dropout(0.8))
-model.add(layers.GRU(64))
+model.add(layers.GRU(64, return_sequences=True))
+model.add(layers.Dropout(0.6))
+model.add(layers.GRU(32))
+model.add(layers.Dense(16, activation="relu"))
 model.add(layers.Dense(5, activation="softmax"))
-model.summary()
+model.summary(print_fn=logging.info)
 
 # Compile the model
 model.compile(
@@ -104,22 +141,22 @@ history = model.fit(
 
 # Checking accuracies
 def render_history(history):
+    plt.figure()
     plt.plot(history["loss"], label="loss")
     plt.plot(history["val_loss"], label="val_loss")
     plt.legend()
     plt.title("Losses")
-    plt.show()
-    plt.close()
+    plt.savefig("temp/" + Sim_ID + "-Loss.jpg")
 
+    plt.figure()
     plt.plot(history["accuracy"], label="accuracy")
     plt.plot(history["val_accuracy"], label="val_accuracy")
     plt.legend()
     plt.title("Accuracies")
-    plt.show()
-    plt.close()
+    plt.savefig("temp/" + Sim_ID + "-Acc.jpg")
 
 render_history(history.history)
 
-# Prediction example
-pred = model.predict( np.expand_dims(X_train[0], axis=0) )
-print(f"Pred {pred}: Real Label {y_train[0]}")
+_, accuracy = model.evaluate( X_val, y_val)
+print(f"Accuracy is {round(accuracy * 100, 2)}%")
+logging.info(f"Accuracy is {round(accuracy * 100, 2)}%")
