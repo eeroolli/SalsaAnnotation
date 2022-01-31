2209ecd57ee061b6075320be666979e25c53d0c6
Eero Olli M720 <eero.olli@gmail.com>
2022-01-31 15:26:32 +0100

    feat(config) read multiline items as lists from config

---
+++ src/example_of_reading_a_list_from_config.py
@@ -0,0 +1,22 @@
+from utils import keep_only_words, make_list_from_string 
+from configparser import ConfigParser, ExtendedInterpolation
+cfg = ConfigParser(interpolation=ExtendedInterpolation())
+
+
+cfg.read('src/config.ini')
+
+# This allows for a very sloppy notation in config ini.  It will accept almost anything and give
+# it back as a list as long as there is some kind of separator between values.
+coreo = make_list_from_string(
+         keep_only_words(
+          cfg.get("annotation", "coreo")))
+
+print(coreo)
+print(mydict)
+
+
+op_columns = make_list_from_string(
+              keep_only_words(
+               cfg.get("openpose", "columns")))
+
+print(op_columns)
---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import re
+
+def keep_only_words(messy_string):
+    # keep only words, dash and white space
+    clean_string = re.sub(r'[^\w\s-]', '', messy_string)
+    return clean_string
+
+
+def make_list_from_string(string):
+    new_list = keep_only_words(string).split()
+    return new_list
+
+
+# Does not work. the function does not get the read config file from the file call the function.
+# def get_list_from_config(section, item):
+#     import re
+#     from configparser import ConfigParser, ExtendedInterpolation
+#     cfg = ConfigParser(interpolation=ExtendedInterpolation())
+#     # this requires that cfg.read('the config file') has been previously run
+#     messy_string = cfg.get(section, item)
+#     # keep only words and white space
+#     clean_string = re.sub(r'[^\w\s]', '', messy_string)
+#     new_list = clean_string.split()
+#     return new_list
