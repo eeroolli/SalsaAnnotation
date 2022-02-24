492148f55347b5e749a33e6f1286cee23d16e814
Eero Olli M720 <eero.olli@gmail.com>
2022-02-24 12:27:11 +0100

    func(upload) added link to FAQ

--- deployment/upload_file_to_S3.py
+++ deployment/upload_file_to_S3.py
@@ -39,13 +39,16 @@ st.title('SalsaAnnotation')
 
 col1, col2, col3 = st.columns([3, 3, 2])
 
-st.col1.markdown("  ")                                                                                               ")
+#st.col1.markdown("  ")  
 st.col1.markdown("This app will allow you to upload a video. You will in 10 minutes receive an email with a link to a videofile that contains your processed video.  ")
-st.col1.markdown("   ")
+#st.col1.markdown("   ")
 st.col1.subheader("Choreographies:")
 st.col1.markdown("[The first choreography]: https://drive.google.com/file/d/1tX5dczXymc4EjAB0A9-5mkPx-pvV412n/view?usp=sharing  [The first choreography] ")       
 st.col1.markdown("The Second Choreography is not out yet")
 st.col1.markdown("At this stage we can only annotate videos that contain one of the predefined choreograpies. ")
+st.col1.subheader("FAQ")
+st.col1.markdown("[FAQ]: https://salsa.eero.no [FAQ]") 
+      
 
 
 
