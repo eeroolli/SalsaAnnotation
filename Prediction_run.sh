602ecfd3984e6bbb819a319a727cdbab4d63a8e3
abergues <berguesana@gmail.com>
2022-03-01 23:10:11 +0100

    Ana: Update PredictionWF

--- Prediction_run.sh
+++ Prediction_run.sh
@@ -6,7 +6,7 @@ then
 elif [ "$1" == "predict" ]
 then
    cd src
-   python Prediction_pipeline.py --video $2
+   python Prediction_pipeline.py --video $2 --coreo $3 --sample $4
    cd .. 
 else 
    echo "Choose an option"
--- src/Prediction_pipeline.py
+++ src/Prediction_pipeline.py
@@ -5,44 +5,72 @@ cfg = ConfigParser(interpolation=ExtendedInterpolation())
 cfg.read('config.ini')
 import global_
 import pandas as pd
+import subprocess
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Prediction script')
     parser.add_argument(
         '--video', required=True,
         help='Video_ID', type=str)
+    parser.add_argument(
+        '--coreo', required=True,
+        help='Indice', type=str)
+    parser.add_argument('--sample', choices=('True', 'False'))
 
     return parser.parse_args()
 
+
 args = parse_args()
 global_.video_id = args.video
+global_.ind_coreo = args.coreo
 global_.PATH_OUTPUT = cfg.get('folders', 'output_predict')
+global_.PATH_OUTPUT_OP = cfg.get('folders', 'output_predict_OP')
 global_.PATH_OUTPUT = global_.PATH_OUTPUT + "_" + global_.video_id
+global_.sample = args.sample
+
+if global_.sample == "True":
+    global_.PATH_OUTPUT_OP = "../Sample-skeletonVideos/" + global_.video_id
+else:
+    global_.PATH_OUTPUT_OP = global_.PATH_OUTPUT_OP + "_" + global_.video_id
+
+
 
 if __name__ == '__main__':
     # TODO here, scripts of Eero for resizing and creating the folder structure
-    # Create json file etc
-    # It assumes that after this step the resize fideo with format
+    # TODO: if sample, not necessary to run the resize part
+
     if not os.path.isdir(global_.PATH_OUTPUT):
         os.makedirs(global_.PATH_OUTPUT)
     else:
         print("Folder already exists ...")
 
-    # TODO: define the body parts of open pose in a single file
+    global_.video_size = "h920" # this number shoulb be return from the function resize
 
-    # import Click_Frame        # Manual assignment of star-end of a figure
+    # Manual annotation
+    import Click_Frame        # Manual assignment of star-end of a figure
 
     # Transformation of assignment to json format
-    run_annot = "python AnnotationFile_t.py "\
-                + global_.PATH_OUTPUT
-    os.system(run_annot)
+    # TODO: better with import?
+    # run_annot = "python AnnotationFile_t.py "\
+    #             + global_.PATH_OUTPUT \
+    #             + " " \
+    #             + global_.ind_coreo
+    # print(run_annot)
+    # os.system(run_annot)
+
+    subprocess.run(["python","AnnotationFile_t.py",global_.PATH_OUTPUT, global_.ind_coreo])
 
     # import OpenPose_run     # Running Openpose, renaming json files
+    # TODO: if sample, not needed to run Openpose
+    # TODO: Openpose out-put (json and mp4) in Open_Pose outpout or sample folder
 
     # Create data frame from json files and Annotation.json
+    # TODO: replace by subprocess
     Anot_file = global_.PATH_OUTPUT + "/Annotation.json"
     run_parsing = "python Parsing-Openpose-Annotation.py " \
                   + global_.video_id \
+                  + " " \
+                  + global_.PATH_OUTPUT_OP \
                   + " " \
                   + global_.PATH_OUTPUT \
                   + " " \
