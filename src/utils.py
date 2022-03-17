76ab57ba9163a301be1672c78f10c5b6d073e9c7
Eero Olli M720 <eero.olli@gmail.com>
2022-03-17 21:29:30 +0100

    (utils) cleaning code

--- src/utils.py
+++ src/utils.py
@@ -10,16 +10,3 @@ def make_list_from_string(string):
     new_list = keep_only_words(string).split()
     return new_list
 
-    dict()
-
-# Does not work. the function does not get the read config file from the file call the function.
-# def get_list_from_config(section, item):
-#     import re
-#     from configparser import ConfigParser, ExtendedInterpolation
-#     cfg = ConfigParser(interpolation=ExtendedInterpolation())
-#     # this requires that cfg.read('the config file') has been previously run
-#     messy_string = cfg.get(section, item)
-#     # keep only words and white space
-#     clean_string = re.sub(r'[^\w\s]', '', messy_string)
-#     new_list = clean_string.split()
-#     return new_list
