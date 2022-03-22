421f5ee4da9763649210b3f513c039eb7a82d939
Eero Olli M720 <eero.olli@gmail.com>
2022-03-22 13:10:06 +0100

    (Streamlit) removed .. from src

--- deployment/collect_and_process_videos.py
+++ deployment/collect_and_process_videos.py
@@ -45,10 +45,10 @@ from os.path import splitext
 from re import split              # regular expression string splitter
 
 # these are saved into script_path. import first after sys.path is changed.
-from ..src.VideoProcessing  import  check_path, stop_if_no_path 
+from src.VideoProcessing  import  check_path, stop_if_no_path 
 # check also load_video_run_openpose(), which still has some bugs.
-from ..src.VideoProcessing import load_video_run_openpose
-from ..src.VideoProcessing import resize_video, delete_outputs, rename_json  
+from src.VideoProcessing import load_video_run_openpose
+from src.VideoProcessing import resize_video, delete_outputs, rename_json  
 
 ### getting some parameters
 
