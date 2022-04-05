d191b51de772d435718181f62c1a97fe67589735
Eero Olli M720 <eero.olli@gmail.com>
2022-04-05 20:08:53 +0200

    Revert "fix (streamlit) path to weights"
    This reverts commit 8906038bbef06b00bd1b24c276eaff03d61bf369.

--- Inference_in_streamlit.py
+++ Inference_in_streamlit.py
@@ -17,7 +17,7 @@ PATH_DATA_VAL = "./ml/models/Data_train_validate/Data_val_norm.csv"
 data_val = pd.read_csv(PATH_DATA_VAL)
 
 # loading the model
-loaded_model = pickle.load(open("./deployment/weights/GRU_model.pkl", 'rb'))
+loaded_model = pickle.load(open("./development/weights/GRU_model.pkl", 'rb'))
 
 # Fixing format of the label
 def enc_label(label):
