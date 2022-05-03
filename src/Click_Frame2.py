5ea60afaec9655ea3ff0a3331a64df2a36b9c770
Eero Olli M720 <eero.olli@gmail.com>
2022-05-03 14:07:18 +0200

    refact (manual_click_frame) now a function

--- src/Click_Frame2.py
+++ src/Click_Frame2.py
@@ -4,96 +4,55 @@ run this file from Repo root with command
 $ python ./src/Click_Frame2.py
 
 """
-import cv2
-import pandas as pd
 import os
-import time
 
 # from global_ import video_id, PATH_OUTPUT, video_size, sample
 
 from configparser import ConfigParser, ExtendedInterpolation
+from utils import manual_click_frame, check_path, get_choreography
+
 cfg = ConfigParser(interpolation=ExtendedInterpolation())
 cfg.read('src/config.ini')
-wait_ms = 30
+wait_ms = 40
 
 ROOT_DIR = cfg.get('installation', 'root_path')
 
 # We could use the openpose.mp4 stickfigure, but it is difficult to be precise.
 # The manual annotation is more precise using the non-stickfigure video.
+#TODO: Add a option to give the file name as 
 video_size =cfg.get('resize_video', 'video_size') 
 video_id = cfg.get("click_frame", 'video_id')
 video_name = video_id + "_" + video_size + ".mp4"
 input_video_resized_dir = cfg.get('click_frame', 'input_video_resized_dir')
 VIDEO_FILE = os.path.join(input_video_resized_dir, video_name)
 
+#TODO: make a list of videos in the folder and compare it with the list of csv files
+#TODO: show only those files that are still missign a csv file.
+
 if os.path.isfile(VIDEO_FILE): 
     print("Input video is OK.")
 else:
     print(f"Can't find {VIDEO_FILE}")
 
+choreo = ""
+choreo = get_choreography("coreo")
+print(choreo)
+number_of_clicks_per_choreo = len(choreo)+1
+print(f"\n  Based on the choreo saved in config.ini")
+print(f"  the correct number of clicks per choreo is {number_of_clicks_per_choreo}.")
+
 
 file_name = cfg.get('output_data', 'click_data')
 #PATH_OUTPUT = os.path.join(input_video_resized_dir, video_id)
-PATH_OUTPUT = cfg.get('click_frame', 'output_dir')
-
+PATH_OUTPUT = os.path.join(cfg.get('click_frame', 'output_dir'), video_id)
 PATH_ANNOT = os.path.join(PATH_OUTPUT, file_name)
 
-# Functions to get the state of the mouse
-def print_frame(event, x, y, flags, *userdata):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        time_list.append(cap.get(cv2.CAP_PROP_POS_MSEC))
-        frame_list.append(cap.get(cv2.CAP_PROP_POS_FRAMES))
-
-print("  Now you need to manually click on the image of the dancer to")
-print("  mark where the choreography STARTS, every 1 beat (of 123-567-),")
-print("  and where each repetition of choreography ends. Eleven clicks ")
-print("  for a choreography, turn, new eleven clicks and so fort.")
-time.sleep(4)
-print("  Grab your mouse and get ready.")
-print("  press q to quit. ")
-
-time.sleep(1)
+############################### 
 
 
-
-# Extracting the frames of a video to feed MediaPipe
-cap = cv2.VideoCapture(VIDEO_FILE)
-time_list = []
-frame_list = []
-
-while (cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame', gray)
-        cv2.setMouseCallback("frame", print_frame)
-
-        if cv2.waitKey(wait_ms) & 0xFF == ord('q'):
-            break
-    else:
-        break
-cap.release()
-cv2.destroyAllWindows()
-
-time_list = [round(x, 2) for x in time_list]
-data_annot = pd.DataFrame(
-    {"Time_ms": time_list,
-     "Frame": frame_list}
-)
-
-# Check if the number of clicks is correct for the choreography.
-#TODO: this should be a function of the choregraphy used and not a fixed number.
-correct_number_of_clicks = 11
-# sometimes we use videos with only one repetition of the choreography - like vier from left.
-# therefore the correct number is not always multiplied by 4.
-if len(data_annot) % correct_number_of_clicks == 0:
-    print("number of clicks seems correct")
-    data_annot.to_csv(PATH_ANNOT, index=False)   #overwrites existing csv file.
-else:
-    print(f"You did {len(data_annot)} clicks. The number should be divisible by {correct_number_of_clicks}")
-    print("your results are not saved. Please rerun the script for this file.") 
-# os.makedirs(PATH_OUTPUT)
-
+manual_click_frame(VIDEO_FILE = VIDEO_FILE,
+                   OUTPUT_CSV_FILE= PATH_ANNOT,
+                   video_id = video_id)
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,70 @@
-import streamlit as st
+
+def manual_click_frame(VIDEO_FILE , 
+                       OUTPUT_CSV_FILE, 
+                       video_id,
+                       number_of_clicks_per_choreo=11,
+                       wait_ms = 40):
+    import cv2
+    import time
+    import pandas as pd
+    
+    # Functions to get the state of the mouse
+    def print_frame(event, x, y, flags, *userdata):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            time_list.append(cap.get(cv2.CAP_PROP_POS_MSEC))
+            frame_list.append(cap.get(cv2.CAP_PROP_POS_FRAMES))
+
+    print(f"\n  ********************* \n  {video_id} \n  *********************")
+    print("  Now you need to manually click on the image of the dancer to")
+    print("  mark where the choreography STARTS, every 1st beat (count 123-567-),")
+    print(f"  and where each repetition of a choreography ENDS. {number_of_clicks_per_choreo} clicks ")
+    print(f"  for a choreography, turn, new {number_of_clicks_per_choreo} clicks and so fort.")
+    time.sleep(4)
+    print("  Grab your mouse and get ready.")
+    print("  press q to quit. \n")
+    time.sleep(1)
+
+
+    # Extracting the frames of a video to feed MediaPipe
+    cap = cv2.VideoCapture(VIDEO_FILE)
+    time_list = []
+    frame_list = []
+
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('frame', gray)
+            cv2.setMouseCallback("frame", print_frame)
+
+            if cv2.waitKey(wait_ms) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+    time_list = [round(x, 2) for x in time_list]
+    data_annot = pd.DataFrame(
+        {"Time_ms": time_list,
+        "Frame": frame_list}
+    )
+
+    # Check if the number of clicks is correct for the choreography.
+    #TODO: this should be a function of the choregraphy used and not a fixed number.
+
+    # sometimes we use videos with only one repetition of the choreography - like vier from left.
+    # therefore the correct number is not always multiplied by 4.
+    if len(data_annot) % number_of_clicks_per_choreo == 0:
+        print("The number of clicks seems correct.")
+        data_annot.to_csv(OUTPUT_CSV_FILE, index=False)   #overwrites existing csv file.
+        return True
+    else:
+        print(f"  You did {len(data_annot)} clicks. The number should be divisible by {number_of_clicks_per_choreo}")
+        print(f"  your results are NOT saved. Please rerun the script for this file.") 
+        return False
+
+
 
 def check_path(x):
   if os.path.isdir(x):
@@ -14,8 +80,6 @@ def stop_if_no_path(x):
   else:
     raise Exception("Problem: There was no ", x)  # Throws an error and stops
 
-
-
 def keep_only_words(messy_string):
     import re
     # keep only words, dash and white space
@@ -30,6 +94,7 @@ def make_list_from_string(string):
     return new_list
 
 
+import streamlit as st
 @st.cache()
 def get_choreography(name_of_choreography):
     from configparser import ConfigParser, ExtendedInterpolation
@@ -42,7 +107,7 @@ def get_choreography(name_of_choreography):
     ch = cfg.get('annotation', name_of_choreography)
     ch = re.sub(r'\[|\]|\"|\'', '', ch)
     ch = re.sub(r'^\s|\s$', '', ch).split(sep=",")
-    print(ch)
+    # print(ch)
     chore = []
     for item in ch:
         chore = chore + [item, item]
